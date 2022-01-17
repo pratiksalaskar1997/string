31e069a91d4b0dbcb7e610f56e656d33dcc1abd9
Pratik Salaskar <95956963+pratiksalaskar1997@users.noreply.github.com>
2022-01-17 09:27:36 +0700

    Update str.py

--- str.py
+++ str.py
@@ -1,4 +1,46 @@
 
+# find length of the string in 3 ways:
+#1 len():
+d = 'hello my name is pratik'
+print(len(d))
+
+#2 for loop:
+d = 'hello my name is pratik'
+c = 0
+for i in d:
+    c += 1
+print(c)
+
+#3 while loop:
+#def findLen(str):
+str = 'hello my name is pratik'
+c = 0
+while str[c:]:
+    c += 1
+#return c
+
+print(c)
+# find length of the string in 3 ways:
+#1 len():
+d = 'hello my name is pratik'
+print(len(d))
+
+#2 for loop:
+d = 'hello my name is pratik'
+c = 0
+for i in d:
+    c += 1
+print(c)
+
+#3 while loop:
+#def findLen(str):
+str = 'hello my name is pratik'
+c = 0
+while str[c:]:
+    c += 1
+#return c
+
+print(c)
 ====================================================
 #WAP to print even length word in a string:
 
